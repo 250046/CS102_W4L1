a2f5084cf1011429b7138d5c23d6d526b12f32a9
Asadbek <asadikedu@gmail.com>
2025-09-11 10:44:28 +0500

    new one

--- main.cpp
+++ main.cpp
@@ -1,57 +1,188 @@
-#include <iostream>
-using namespace std;
+#include <iostream> // for output-input
+#include <iomanip>
+#include <string>
+#include <cmath>
+#include <math.h>
+
+using namespace std; // cout, cin, vector - these are inside and we write it in order to avoid writing it twice.
+
 int main() {
-    // Problem 1
-    int intNumber = 18;
-    float floatNumber = 3.14159;
-    double doubleNumber = 45.1234;
-    bool boolean = true;
-    char charName = 'A';
-    cout << "...:::: Problem 1 Solutions ::::..." << endl;
-    cout << "The value of intNumber is " << intNumber << ", size is: " << sizeof(intNumber) << endl;
-    cout << "The value of floatNumber is " << floatNumber << ", size is: " << sizeof(floatNumber) << endl;
-    cout << "The value of doubleNumber is " << doubleNumber << ", size is: " << sizeof(doubleNumber) << endl;
-    cout << "The value of boolean is " << boolean << ", size is: " << sizeof(boolean) << endl;
-    cout << "The value of charName is " << charName << ", size is: " << sizeof(charName) << endl;
+    // Task - 1
 
-    // Problem 2
-    string myName = "Asadbek Abduxalilov";
-    int myIdNumber = 250046;
-    string favSong = "It was a good - Ice Qube";
-    string favMovie = "Avengers: End Game";
+    // int intNumber = 18;
+    // float floatNumber = 3.14159;
+    // double doubleNumber = 45.1234;
+    // bool boolean = true;
+    // char charName = 'A';
+    // cout << "The intNumber is " << intNumber << ", size is " << sizeof(intNumber) << endl;
+    // cout << "The floatNumber is " << floatNumber << ", size is " << sizeof(floatNumber) << endl;
+    // cout << "The doubleNumber is " << doubleNumber << ", size is " << sizeof(doubleNumber) << endl;
+    // cout << "The boolean is " << boolean << ", size is " << sizeof(boolean) << endl;
+    // cout << "The charName is " << charName << ", size is " << sizeof(charName) << endl;
+    // return 0;
 
-    cout << "...:::: Problem 2 Solutions ::::..." << endl;
-    cout << myName << endl;
-    cout << myIdNumber << endl;
-    cout << favSong << endl;
-    cout << favMovie << endl;
+    // Task - 2
+    // string myName = "Asadbek Abdukhalilov";
+    // int myID = 233242;
+    // string favSong = "It was a good day - Ice Qube";
+    // string favMovie = "Avatar:2";
 
-    // Problem 3
-    cout << "...:: Problem 3 Solutions ::::..." << endl;
-    cout << "\"I'm standing\" on the edge of some crazy cliff." << endl;
-    cout << "What I have to do, I have to catch everybody \" " << endl;
-    cout << " if they start to go over the cliff--I mean\" " << endl;
-    cout << "\t\\if they're running and they don't look " << endl;
-    cout << "\twhere they're going I have to come out" << endl;
-    cout << "\t\\from somewhere and     catch them" << endl;
+    // cout << myName << endl;
+    // cout << myID << endl;
+    // cout << favSong << endl;
+    // cout << favMovie << endl;
+    // return 0;
 
-    // Problem 4
-    cout << "...:: Problem 4 Solutions ::::..." << endl;
-    int a = 189;
-    char b = 'B';
+    // Task - 3
+    // cout << "\"I'm standing\" on the edge of some crazy cliff." << endl;
+    // cout << "What I have to do, I have to catch everybody\\" << endl;
+    // cout << " if they start to go over the cliff—I mean \\" << endl;
+    // cout << "\t\\if they're running and they don't look" << endl;
+    // cout << "\twhere they're going I have to come out" << endl;
+    // cout << "\tfrom somewhere and     catch them." << endl;
+    // return 0;
 
-    //1
-    float floatNumber2 = a/3;
-    //2
-    int decimal_value = static_cast<int>(b);
-    float numOper = (a + decimal_value)/5;
-    //3
-    int positions = a/10 -1;
-    char result = b + positions;
+    // Task - 4
+    // int a = 13;
+    // char b = 'R';
+    // float process1 = a / 3.0f;
+    // cout << fixed << setprecision(1) << process1 << endl;
+    // cout << (int)b << endl;
+    // cout << (((a + (int)b)/5)) << endl;
+    // cout << char((int)b + (a/10-1)) << endl;
+    // return 0;
 
-    cout << floatNumber2 << endl;
-    cout << decimal_value << endl;
-    cout << numOper << endl;
-    cout << result << endl;
-    return 0;
-}
+    // Task - 5
+    // int a = 44;
+    // int b = 12;
+    // cout << "Sum = " << a + b << endl;
+    // cout << "Product = " << a * b << endl;
+    // return 0
+
+    // Task - 6
+    // int value1;
+    // int value2;
+
+    // cout << "Enter first value: ";
+    // cin >> value1;
+
+    // cout << "Enter second value: ";
+    // cin >> value2;
+
+    // int sum = value1 + value2;
+    // cout << "Sum: " << value1 << " + " << value2 << " = " << sum << endl;
+    // return 0;
+
+    // Task 7
+    // int x;
+    // cin >> x;
+    // int y;
+    // cin >> y;
+    // float t = (3*((x*x)+3))/((y/4)+3);
+    // cout << "t = " << t << endl;
+    // return 0;
+
+    // Task 8
+    /*
+    
+    Area of sphere is found with formula: A = π * r^2
+
+     */
+    // float diameter;
+    // float pii = 3.14;
+    // cout << "Enter a diameter: " << endl;
+    // cin >> diameter;
+
+    // float result =  4 * pii * pow((diameter/2),2);
+
+    // cout << "The result is: " << result << endl;
+    // return 0;
+
+    // // Task 9
+    // float v1;
+    // cout << "Enter starting velocity value: " << endl;
+    // cin >> v1;
+    // float v2;
+    // cout << "Enter ending velocity value: " << endl;
+    // cin >> v2;
+    // float time;
+    // cout << "Enter time span in seconds: " << endl;
+    // cin >> time;
+
+    // float result = (v2 - v1)/(time);
+
+    // cout << "Average accelartion is " << result << endl;
+    // return 0;
+
+    // Task 10
+    // float distance;
+    // cin >> distance;
+    // float fuel;
+    // cin >> fuel;
+    // float price;
+    // cin >> price;
+
+    // float cost = (distance/fuel)*price;
+    // cout << "The cost of driving is $" << cost << endl;
+    // return 0;
+
+    // Task 11
+    // double value1;
+    // cin >> value1;
+    // double value2;
+    // cin >> value2;
+
+    // cout << value1 << " " << value2 << endl;
+    // return 0;
+
+    // Task 12
+    // int total_students;
+    // cout << "Enter total: " << endl;
+    // cin >> total_students;
+    // int groups = 11;
+    // int first_group = total_students / groups;
+    // int last_group = total_students % groups;
+    // cout << "In first gorup: " << first_group << endl;
+    // cout << "In last group: " << last_group << endl;
+    // return 0;
+
+    // Task 13
+    // float side;
+    // cout << "Enter side of hexagon: " << endl;
+    // cin >> side;
+    // float area_h = ((3 * sqrt(3))/2)*(pow(side, 2));
+    // cout << "Area of hexagon: " << area_h << endl;
+    // return 0;
+
+    // Task 14
+    // int num;
+    // cout << "Enter a number: " << endl;
+    // cin >> num;
+    // num = num + 1;
+    // num = num * 3;
+    // num = num - 14;
+    // num = num / 4;
+    // num = num - 1;
+    // num = num % 9;
+    // cout << num << endl;
+    // return 0;
+
+    // Task 15
+    // char charName = 'A';
+    
+    // int H_char = (int)charName + 7;
+    // char h_ch = char(H_char);
+    
+    // char E_char = (int)charName + 4;
+    // char e_ch = char(E_char);
+
+    // char L_char = (int)charName + 11;
+    // char l_ch = char(L_char);
+
+    // char O_char = (int)charName + 14;
+    // char o_ch = char(O_char);
+
+    // char sign_char = (int)charName - 32;
+    // char sign_ch = char(sign_char);
+    // cout << h_ch << e_ch << l_ch << l_ch << o_ch << sign_ch << endl;
+}   
